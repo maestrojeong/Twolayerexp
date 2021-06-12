1dc8602fa20027a9dc4ee258b56e01f1eca3b7c0
maestrojeong <aadd4020@gmail.com>
2021-06-13 02:12:30 +0900

    jyw

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = x.view(-1, 28*28)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
@@ -31,46 +31,34 @@ class Net(nn.Module):
 def test(model, device, dataloader):
     model.eval()
     test_loss = 0
-    test_acc = 0
     with torch.no_grad():
         for data, target in dataloader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            test_acc += pred.eq(target.view_as(pred)).sum().item()
-
     test_loss /= len(dataloader.dataset)
-    test_acc /= len(dataloader.dataset)
-
     model.train()
-
-    return test_loss, test_acc
+    return test_loss
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--lr', type=float, default=1.0, help='learning rate (default: 1.0)')
-    parser.add_argument('--boarddir', type=str, default='./board', help='tensorboard directory')
-    args = parser.parse_args()
+    #testset = datasets.MNIST(DATADIR, train=False, download=False, transform=transform)
+    #test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
+    lr =1.0
+    boarddir = './board'
+    batch_size = 10000
 
     transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-
-    DATADIR = "/data_large/readonly/"
-    trainset = datasets.MNIST(DATADIR, train=True, download=False, transform=transform)
-    testset = datasets.MNIST(DATADIR, train=False, download=False, transform=transform)
-
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=4)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
+    DATADIR = "./mnist_data/"
+    trainset = datasets.MNIST(DATADIR, train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     device = torch.device("cuda" if use_cuda else "cpu")
     model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-
+    optimizer = optim.Adadelta(model.parameters(), lr=lr)
     ################################################
-    boardsubdir = args.boarddir+'/lr{}/'.format(args.lr)
+    boardsubdir = boarddir+'/lr{}/'.format(lr)
+
     tensorboardwrite = SummaryWriterManager(boardsubdir)
     ################################################
 
@@ -83,24 +71,11 @@ def main():
             loss = F.nll_loss(output, target)
             loss.backward()
             optimizer.step()
-
-        test_loss, test_acc = test(model, device, test_loader)
-        train_loss, train_acc = test(model, device, train_loader)
-
-        print("Test")
-        print('Loss: {:.4f}, Accuracy : {:.4f}'.format(test_loss, test_acc))
+        train_loss = test(model, device, train_loader)
         print("Train")
         print('Loss: {:.4f}, Accuracy : {:.4f}'.format(train_loss, train_acc))
-
-        ################################################
-        write_content = {
-                'val_acc' : test_acc,
-                'val_loss' : test_loss,
-                'train_acc' : train_acc,
-                'train_loss' : train_loss}
+        write_content = {'train_loss' : train_loss}
         tensorboardwrite.add_summaries(write_content, global_step=epoch)
-        ################################################
-        scheduler.step()
 
 if __name__ == '__main__':
     main()
